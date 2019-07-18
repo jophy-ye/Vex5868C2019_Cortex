04f47bbdb442264aa2c5e623861ac6fb84122a8a
jophy <jophyyjh@gmail.com>
2019-07-18 23:55:44 +0800

    Add a Config.h and typedefs.h

---
+++ include/Config.h
@@ -0,0 +1,58 @@
+/**
+ * -----------------------------------------------------------------
+ * 
+ * The Config file of this project. 
+ * It contains user configuration, port_numbers, constants, etc.
+ * 
+ * -----------------------------------------------------------------
+ */
+
+#include <stdbool.h>
+
+
+// all port numbers
+// all not set +++++++++++++++++++++++++++++
+#define LEFT_FRONT_MOTOR_PIN                0
+#define RIGHT_FRONT_MOTOR_PIN               0
+#define LEFT_BACK_MOTOR_PIN                 0
+#define RIGHT_BACK_MOTOR_PIN                0
+
+
+
+// all dimensions of robot
+// all not set +++++++++++++++++++++++++++++
+#define WHEEL_DIAMETER                      0
+/*
+    The turning radius of the robot.
+    TURN_RADIUS = sqrt(robot_width^2 + distance_between_wheel^2) / 2
+*/
+#define ROBOT_TURN_RADIUS                   0
+
+
+
+// all values for PID control
+// all not set +++++++++++++++++++++++
+#define KP                                  3
+/*
+    if OptimizedStop is enabled,
+    the robot will start to slow down 'OPTIMIZEDSTOP_PRESERVE_DIST' cm before the goal
+*/
+#define OPTIMIZEDSTOP_PRESERVE_DIST         5
+
+
+
+// all info about basis motors (except ports)
+// all not set +++++++++++++++++++++++
+#define LEFT_FRONT_MOTOR_REVERSED           false
+#define RIGHT_FRONT_MOTOR_REVERSED          false
+#define LEFT_BACK_MOTOR_REVERSED            false
+#define RIGHT_BACK_MOTOR_REVERSED           false
+
+
+
+// all info of joystick action
+// all not set +++++++++++++++++++++
+// an absolute value for horizontal sliding joystick thresold
+#define JOY_HORIZONTAL_SLIDE_THRESOLD       0.4
+// the motor value would be joystick_input * CONTROL_P_VAL
+#define JOY_CONTROL_P_VAL                   0.95
---
+++ include/typedefs.h
@@ -0,0 +1,25 @@
+#ifndef _TYPEDEFS_H_
+#define _TYPEDEFS_H_
+
+/**
+ * -------------------------------------------------------------------
+ * 
+ * This file contains all of the typedefs we need for the whole program.
+ * 
+ * -------------------------------------------------------------------
+ */
+
+#include <stdbool.h>
+
+
+/**
+ * The 393 Motor type. It is actually containing 
+ * the port of that motor, and if it is reversed.
+ */
+typedef struct
+{
+    unsigned char port;
+    bool reversed;
+} R_Motor_t;
+
+#endif
