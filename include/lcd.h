55fd6355719d556ab83585d0b60714d0550d399f
jophy <jophyyjh@gmail.com>
2019-07-19 21:20:48 +0800

    Add code for LCD display

--- include/lcd.h
+++ include/lcd.h
@@ -11,6 +11,44 @@
 
 #include "API.h"
 
+#include <stdbool.h>
+
+
+// type for the current active screen we are on
+typedef enum {
+    Initialize_Main,    // the home page
+    AutonSelect,        // the page for selecting autonomous
+    AutonConfirm,       // the page for confirming the autonomous selection
+    RobotCheck          // the page for robot check
+} Page_Act_t;
+
+
+/**
+ * Get the 3 buttons status on the LCD
+ * stored accordingly in LCD_LeftBtnStat, LCD_CenterBtnStat, and LCD_RightBtnStat
+ */
+void getLCDBUttonPressed();
+
+/**
+ * the task handler function for handling LCD
+ */
 void LCDTask_Handler();
+
+
+
+/**
+ * Here follows a few functions, 
+ * each used to handle a different screen
+ * 
+ * They will be called by the main loop
+ */
+void Initialize_Main_Handler();
+
+void AutonSelect_Handler();
+
+void AutonConfirm_Handler();
+
+void RobotCheck_Handler();
+
 
 #endif
--- src/lcd.c
+++ src/lcd.c
@@ -10,9 +10,160 @@
 
 #include "lcd.h"
 #include "Config.h"
+#include "typedefs.h"
+
+#include <stdbool.h>
+
+
+bool LCD_LeftBtnStat, LCD_CenterBtnStat, LCD_RightBtnStat;
+bool Screen_Changed;    // set to true if the screen has changed but not yet done anything for it
+Page_Act_t Page_Act;    // the current active page
+
+/** 
+ * values can be: 0 ~ 4, each accordingly be:
+ * RedFront, RedBack, BlueFront, BlueBack, Skills
+ * */ 
+short AutonPos_CurrentSelection;
+const char* AutonPos_Text[] = 
+    { "    RedFront    ", 
+      "     RedBack    ", 
+      "    BlueFront   ", 
+      "    BlueBack    ", 
+      "     Skills     " };
+
+
+void getLCDBUttonPressed()
+{
+    unsigned int LCD_Btn_Stat = lcdReadButtons(LCD_PIN);
+    LCD_LeftBtnStat = LCD_Btn_Stat >> 2;
+    LCD_Btn_Stat -= (LCD_Btn_Stat >> 2) << 2;
+    LCD_CenterBtnStat = LCD_Btn_Stat >> 1;
+    LCD_Btn_Stat -= (LCD_Btn_Stat >> 1) << 1;
+    LCD_CenterBtnStat = LCD_Btn_Stat >> 0;
+}
 
 
 void LCDTask_Handler()
 {
     lcdInit(LCD_PIN);
+    lcdClear(LCD_PIN);
+    Page_Act = Initialize_Main;
+    Screen_Changed = false;
+    AutonPos_CurrentSelection = 0;
+
+    while(true)
+    {
+        getLCDBUttonPressed();
+
+        switch(Page_Act)
+        {
+            case Initialize_Main: Initialize_Main_Handler(); break;
+            case AutonSelect: AutonSelect_Handler(); break;
+            case AutonConfirm: AutonConfirm_Handler(); break;
+            case RobotCheck: RobotCheck_Handler(); break;
+        }
+
+        delay(150);
+    }
+}
+
+
+
+void Initialize_Main_Handler()
+{
+    /**
+     * if the screen has changed, display the message
+     * if not, just detect the buttons
+     */
+    if (Screen_Changed)
+    {
+        lcdSetText(LCD_PIN, 1, " Welcome, 5868C ");
+        lcdSetText(LCD_PIN, 2, "Auton_S    Check");
+        Screen_Changed = false;
+    }
+
+    if (LCD_LeftBtnStat)
+    {
+        // navigate to AutonSelect Page
+        Page_Act = AutonSelect;
+        Screen_Changed = true;
+    }
+    else if (LCD_RightBtnStat)
+    {
+        // navigate to RobotCheck Page
+        Page_Act = RobotCheck;
+        Screen_Changed = true;
+    }
+}
+
+void AutonSelect_Handler()
+{
+    /**
+     * if the screen has changed, display the message
+     * if not, just detect the buttons
+     */
+    if (Screen_Changed)
+    {
+        lcdSetText(LCD_PIN, 2, " <   Confirm  > ");
+        Screen_Changed = false;
+    }
+
+    if (LCD_CenterBtnStat)
+    {
+        Screen_Changed = true;
+        Page_Act = AutonConfirm;
+    }
+    else if (LCD_LeftBtnStat)
+    {
+        AutonPos_CurrentSelection--;
+        AutonPos_CurrentSelection = AutonPos_CurrentSelection % 5;
+    }
+    else if (LCD_RightBtnStat)
+    {
+        AutonPos_CurrentSelection++;
+        AutonPos_CurrentSelection = AutonPos_CurrentSelection % 5;
+    }
+
+    // set the text for the current selection
+    lcdSetText(LCD_PIN, 1, AutonPos_Text[AutonPos_CurrentSelection]);
+}
+
+void AutonConfirm_Handler()
+{
+    /**
+     * if the screen has changed, display the message
+     * if not, just detect the buttons
+     */
+    if (Screen_Changed)
+    {
+        lcdSetText(LCD_PIN, 1, AutonPos_Text[AutonPos_CurrentSelection]);
+        lcdSetText(LCD_PIN, 2, "Sure?   No   Yes");
+        Screen_Changed = false;
+    }
+
+    if (LCD_CenterBtnStat)
+    {
+        Screen_Changed = true;
+        Page_Act = Initialize_Main;
+    }
+    else if (LCD_RightBtnStat)
+    {
+        Screen_Changed = true;
+        Page_Act = Initialize_Main;
+
+        extern AutonPos_t AutonPos;
+        switch (AutonPos_CurrentSelection)
+        {
+            case 0: AutonPos = RedFront; break;
+            case 1: AutonPos = RedBack; break;
+            case 2: AutonPos = BlueFront; break;
+            case 3: AutonPos = BlueBack; break;
+            case 4: AutonPos = Skills; break;
+        }
+    }
+}
+
+void RobotCheck_Handler()
+{
+
 }
