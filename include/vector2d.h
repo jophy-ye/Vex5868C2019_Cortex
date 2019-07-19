a2d2ef1afc0787199a404ff67907503d9be62c65
jophy <jophyyjh@gmail.com>
2019-07-19 13:07:44 +0800

    Moved all function implementaions of vector2d into header

--- include/vector2d.h
+++ include/vector2d.h
@@ -1,6 +1,7 @@
 #ifndef _VECTOR_2D_H_
 #define _VECTOR_2D_H_
 
+#include <math.h>
 
 /**
  * Standard functions and typedefs for Vector 2d
@@ -11,17 +12,30 @@ typedef struct {
 } vector2d;
 
 
-inline void vector2d_set_x(vector2d* vector2d_input, double x_input);
+inline void vector2d_set_x(vector2d* vector2d_input, double x_input)
+{ vector2d_input->x = x_input; }
 
-inline void vector2d_set_y(vector2d* vector2d_input, double y_input);
+inline void vector2d_set_y(vector2d* vector2d_input, double y_input)
+{ vector2d_input->y = y_input; }
 
-inline double vector2d_get_x(vector2d* vector2d_input);
+inline double vector2d_get_x(vector2d* vector2d_input)
+{ return vector2d_input->x; }
 
-inline double vector2d_get_y(vector2d* vector2d_input);
+inline double vector2d_get_y(vector2d* vector2d_input)
+{ return vector2d_input->y; }
 
-inline double vector2d_get_magnitude(vector2d* vector2d_input);
+inline double vector2d_get_magnitude(vector2d* vector2d_input)
+{
+    return sqrt(vector2d_input->x * vector2d_input->x + vector2d_input->y * vector2d_input->y);
+}
 
-double vector2d_get_gradient(vector2d* vector2d_input);
+inline double vector2d_get_gradient(vector2d* vector2d_input)
+{
+    if (vector2d_input->y == 0)
+        return 99999999999; // something we pretend to be inf
+    else
+        return vector2d_input->x / vector2d_input->y;
+}
 
 
 #endif
